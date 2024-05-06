552c42015ad9313b776146c451637f532386c19a
AGAND842 <155474053+AGAND842@users.noreply.github.com>
2024-05-06 14:05:25 +0330

    Update partweb.py

--- partweb.py
+++ partweb.py
@@ -7,17 +7,16 @@ def type_effect(text):
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
-        time.sleep(0.1)
+        time.sleep(0.0)
 
 code = '''
  __        __  ___       ___  __  
 |__)  /\  |__)  |  |  | |__  |__) 
 |    /~~\ |  \  |  |/\| |___ |__) 
                                   
-{───────────────────────────────────────────────────}
-OWNER ~> AGAND842
-What does this tool do? When this tool is run, it takes the address of a site from us, and then when we give it, it shows us all the parts of the site.
-{───────────────────────────────────────────────────}
+{~~~~~~~~~~~~~~~~~~~}
+* OWNER ~> AGAND842 *
+{~~~~~~~~~~~~~~~~~~~}
 '''
 
 
